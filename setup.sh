7deea1975fe8d1e73ae48181908bb7814bc1ef54
Caleb Rabbon <caleb.rabbon@gmail.com>
2025-02-08 00:05:49 -0800

    Added verbose messaging to setup.sh and echo message for start and finish

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-echo "Using ln to create .bashrc symlink as stow version 2.3.1 can't handle dotfiles in ~/.dotfile directly"
-echo "Supposedly fixed in version 2.4.0 https://github.com/aspiers/stow/issues/33"
-ln -s -t ~/ ~/.dotfiles/.bashrc -f
-ln -s -t ~/ ~/.dotfiles/.vimrc -f
-ln -s -t ~/ ~/.dotfiles/.tmux.conf -f
+# Created by Caleb Rabbon 2/7/2025
+# Using ln to create .bashrc symlink as stow version 2.3.1 can't handle dotfiles in ~/.dotfile directly
+# Supposedly fixed in version 2.4.0 https://github.com/aspiers/stow/issues/33
+
+echo -e "\033[0;32mStart\033[0m\n"
+
+ln -s -t ~/ ~/.dotfiles/.bashrc -f -v
+ln -s -t ~/ ~/.dotfiles/.vimrc -f -v
+ln -s -t ~/ ~/.dotfiles/.tmux.conf -f -v
+
+echo -e "\033[0;32m\nFinished creating symlinks\033[0m"
