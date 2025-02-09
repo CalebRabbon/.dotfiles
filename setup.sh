12d485413edaafd2eea2ec18603680851ab3b47d
Caleb Rabbon <caleb.rabbon@gmail.com>
2025-02-08 23:06:04 -0800

    Updated setup.sh script with commands to install vim and tmux plugins automatically via git clone commands

--- setup.sh
+++ setup.sh
@@ -4,10 +4,24 @@
 # Using ln to create .bashrc symlink as stow version 2.3.1 can't handle dotfiles in ~/.dotfile directly
 # Supposedly fixed in version 2.4.0 https://github.com/aspiers/stow/issues/33
 
-echo -e "\033[0;32mStart\033[0m\n"
+echo -e "\033[0;32mStart\033[0m"
 
+echo -e "\033[0;32m\nCloning Vundle plugin for vim\033[0m"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo -e "\033[0;32m\nCloning Tmux Plugin Manager for tmux plugins\033[0m"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+echo -e "\033[0;32m\nCreating symlinks\033[0m"
 ln -s -t ~/ ~/.dotfiles/.bashrc -f -v
 ln -s -t ~/ ~/.dotfiles/.vimrc -f -v
 ln -s -t ~/ ~/.dotfiles/.tmux.conf -f -v
-
 echo -e "\033[0;32m\nFinished creating symlinks\033[0m"
+
+vim +PluginInstall +qall
+echo -e "\033[0;32m\nInstalled Vim Plugins\033[0m"
+
+tmux source ~/.tmux.conf
+echo -e "\033[0;32m\nInstalled Tmux Plugins\033[0m"
+
+echo -e "\033[31mWARNING: Run tmux then run Prefix+I to install the rest of the plugins\033[0m"
