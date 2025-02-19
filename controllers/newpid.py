bf43d04b99395c45852dec66e26743ae5e6a6220
Caleb Rabbon <caleb.rabbon@gmail.com>
2025-02-19 15:56:02 -0800

    Tuned PID controller with p=.2 i=.1 d=-.12 and got total cost of 87.512 for rollout of 100

---
+++ controllers/newpid.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+from . import BaseController
+
+
+class Controller(BaseController):
+    """
+    A simple PID controller
+    """
+
+    def __init__(
+        self,
+    ):
+        self.p = 0.2
+        self.i = 0.1
+        self.d = -0.12
+        self.error_integral = 0
+        self.prev_error = 0
+
+    def update(self, target_lataccel, current_lataccel, state, future_plan):
+        error = target_lataccel - current_lataccel
+        self.error_integral += error
+        error_diff = error - self.prev_error
+        self.prev_error = error
+        return self.p * error + self.i * self.error_integral + self.d * error_diff
